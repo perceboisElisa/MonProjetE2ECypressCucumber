4adab4c830111443a78074293099d8d1322b571d
perceboisElisa <percebois.elisa@gmail.com>
2018-02-24 17:01:24 +0100

    ajout 2eme scenario

--- cypress/integration/Phones.feature
+++ cypress/integration/Phones.feature
@@ -1,7 +1,14 @@
 Feature: The Phones app
   I want to test my AngularJS app using Cucumber and Cypress
-  
-  Scenario: Search the details of a phone
+
+Scenario: Search the details of a phone
     Given a list of phones on phones store
     When I search the phone "Dell Streak 7" in search input
-    Then I should see the overview of "Dell Streak 7" 
+    Then I should see the overview of "Dell Streak 7"
+    Then I should know the title of the page
+  
+
+Scenario: Display a list of phones
+    Given a list of phones on phones store
+    When I display this phones list
+    Then I should get "20" phones
--- cypress/support/step_definitions/phones.js
+++ cypress/support/step_definitions/phones.js
@@ -1,18 +1,35 @@
 const url = 'http://localhost:8000/index.html#!/phones';
 
+//1er scenario
 given('a list of phones on phones store', () => {
   cy.visit(url);
   // Should be on a new URL which includes '/phones'
   cy.url().should('include', '/phones');
 })
-//^a list of phones on "([^"]*)"$/
-//   this.Given(/^a list of phones on "([^"]*)"$/, function (arg1, callback) {
+
 when('I search the phone {string} in search input', (name) => {
-  cy.log ("name "+ name);
   // Get an input, type into it and verify that the value has been updated
-  cy.get('input[ng-model="$ctrl.query"]').type(name).should('have.value', name)
+  cy.get('input[ng-model="$ctrl.query"]').type(name).should('have.value', name);
 })
 
 then('I should see the overview of {string}', (name) => {
-   cy.contains(name).click()
+   cy.contains(name).click();
 })
+
+then('I should know the title of the page', () => {
+ cy.title().should('include', 'Google Phone Gallery');
+})
+
+//2eme scenario
+when('I display this phones list', () => { 
+  //ng model
+  cy.ng('model', '$ctrl.query')
+})
+
+then('I should get {string} phones', (number) => {
+ //cy.log(number); 	
+ cy.get('ul.phones li').should('have.length', number)
+})
+
+
+
